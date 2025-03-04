7d8ed8d4eb7d2c296ac87fd44d1f516249a72b31
Yaasiin <ayevayaasiin@gmail.com>
2025-03-04 09:06:42 +0000

    video calling ok

--- lib/config/agora_config.dart
+++ lib/config/agora_config.dart
@@ -1,5 +1,5 @@
 class AgoraConfig {
-  static const String appId = "aa089bca21ed4500b9bca91355fecbfb"; // Replace with your Agora App ID
+  static const String appId = "aa089bca21ed4500b9bca91355fecbfb"; // Replace with your Agora App ID, Don't use mine, it'll be removed soon
   static const String token = "007eJxTYGiVVVXoOJGWqidkeun91Neat0QLV7XGV6jNt5Gbp8L/VFSBITHRwMIyKTnRyDA1xcTUwCAJxLE0NDY1TUtNTkpLWrrlWHpDICNDoUY2KyMDBIL4ggzJiTk58SWpxSXxyRmJeXmpOQwMAGkJI0s="; // Use null for testing or add your token here
-  static const String channelName = "call_test_channel"; // Default channel name
+  static const String channelName = "call_test_channel"; // Default channel name for testing
 }
